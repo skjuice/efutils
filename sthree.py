f04c015351b008389fd8aa79ec70966b96a897c3
Skjuice <nodesynth@gmail.com>
2019-12-23 12:12:00 -0500

    wip

--- sthree.py
+++ sthree.py
@@ -9,12 +9,14 @@ Give a filepath and it will upload to the bucket
 class Sthree:
 
     def __init__(self, key, secret, bucket):
+        self.bucket_name = bucket
         session = boto3.Session(
             aws_access_key_id=key,
             aws_secret_access_key=secret,
         )
         s3_resource = session.resource('s3')
         self.bucket = s3_resource.Bucket(name=bucket)
+        self.client = session.client('s3')
         self.logger = logging.getLogger(__name__)
 
     def upload(self, file_path, object_key):
@@ -27,3 +29,11 @@ class Sthree:
 
     def download(self, object_key, local_filepath):
         self.bucket.download_file(object_key, local_filepath)
+
+    def check_resource_exists_at_key(self, key):
+        try:
+            self.client.head_object(Bucket=self.bucket_name, Key=key)
+            return True
+        except ClientError:
+            # Not found
+            return False
